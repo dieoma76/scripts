1e893226afc97befd01837e3766fdab4e9f42970
dieoma76 <dieoma@gmail.com>
2023-02-02 12:59:56 +0100

    csv2json Python Script um eine CSV einzulesen, Felder umzubenennen, neu zusammenzusetzen und als JSON File neu zu schreiben

--- csv2json.py
+++ csv2json.py
@@ -1,7 +1,7 @@
 # Kleines Python Script um eine CSV einzulesen, Felder umzubenennen, neu zusammenzusetzen und
 # als JSON File neu zu schreiben
 # copyright: Markus Smieja
-# 02.02.2023
+# 02.02.2023, v0.1
 
 import csv 
 import json 
