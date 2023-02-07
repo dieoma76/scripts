748db43117d21097992950df9e53bafb29ae1cbe
dieoma76 <dieoma@gmail.com>
2023-02-07 09:45:10 +0100

    Changes to match nex export format of CSV File

--- backup/csv2json.py
+++ backup/csv2json.py
@@ -1,13 +1,13 @@
 # Kleines Python Script um eine CSV einzulesen, Felder umzubenennen, neu zusammenzusetzen und
 # als JSON File neu zu schreiben
 # copyright: Markus Smieja
-# 07.02.2023, v0.2
+# 02.02.2023, v0.1
 
 import csv 
 import json 
 
-csvFilePath = r'VMware Kochbuch Library.csv'
-jsonFilePath = r'VMware Kochbuch Library.json'
+csvFilePath = r'Cookbook.csv'
+jsonFilePath = r'Cookbook.json'
 
 def csv_to_json(csvFilePath, jsonFilePath):
     jsonArray = []
@@ -21,20 +21,19 @@ def csv_to_json(csvFilePath, jsonFilePath):
         for row in csvReader: 
             #add this python dict to json array
             jsonArray.append(row)
+            del row['Bild des Autors']
             row['@context'] = 'https://schema.org'
             row['@type'] = 'Recipe'
             del row['E-Mail']
+            del row['Copyright Bestätigung']
             row['author'] = row['Vorname'] + " " + row['Name']
             del row['Vorname']
             del row['Name']
-            row['cookTime'] = row.pop('Zubereitungsdauer')
-            row['description'] = row.pop('Geschichte des Rezepts')
-            row['image'] = row.pop('Rezeptbilderpfad')
+            row['description fuer Jochen'] = row.pop('Geschichte des Rezepts')
+            row['image'] = row.pop('Bilder des Gerichts')
             row['name'] = row.pop('Name des Rezepts')
-            del row ['Kategorie']
             row['recipeIngredient'] = row['Zutatenliste'].split(',')
             del row['Zutatenliste']
-            del row['Autorbilderpfad']
             row['recipeInstructions'] = row.pop('Zubereitung')
 
     #convert python jsonArray to JSON String and write to file
